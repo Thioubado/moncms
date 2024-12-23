88c77e64af6b239c0c820de65ce369a1038fe363
Momo <seckndanane16@gmail.com>
2024-12-23 23:43:03 +0100

    Les données: Les relations

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
@@ -10,5 +11,11 @@ class Category extends Model
     public $timestamps = false;
 
     protected $fillable = ['title', 'slug'];
+
+    // Les relations
+    public function Posts(): HasMany
+    {
+        return $this->hasmany(Post::class);
+    }
 
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -24,6 +25,12 @@ class User extends Authenticatable
         'role',
         "valid",
     ];
+
+    // Les relations
+    public function posts(): HasMany
+    {
+        return $this->hasMany(Post::class);
+    }
 
     /**
      * The attributes that should be hidden for serialization.
